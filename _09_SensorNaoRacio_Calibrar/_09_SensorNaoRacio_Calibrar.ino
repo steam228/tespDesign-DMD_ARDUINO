c1314df19daf6757cf807feecc072c9cfa45b544
André Rocha <steam228@gmail.com>
2016-11-03 11:13:00 +0000

    bug corrected
    dava erro estupido nalguns IDE’s,
    passar double para antes do setup

--- _09_SensorNaoRacio_Calibrar/_09_SensorNaoRacio_Calibrar.ino
+++ _09_SensorNaoRacio_Calibrar/_09_SensorNaoRacio_Calibrar.ino
@@ -13,6 +13,16 @@
 int valorTherm = 0;
 int Temperatura = 0;
 
+double Thermistor(int RawADC) {
+ double Temp;
+ Temp = log(10000.0*((1024.0/RawADC-1))); 
+//         =log(10000.0/(1024.0/RawADC-1)) // for pull-up configuration
+ Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+ Temp = Temp - 273.15;            // Convert Kelvin to Celcius
+ //Temp = (Temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+ return Temp;
+}
+
 
 void setup() {
  Serial.begin(9600);
@@ -35,12 +45,4 @@ delay(20);
 
 
 
-double Thermistor(int RawADC) {
- double Temp;
- Temp = log(10000.0*((1024.0/RawADC-1))); 
-//         =log(10000.0/(1024.0/RawADC-1)) // for pull-up configuration
- Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
- Temp = Temp - 273.15;            // Convert Kelvin to Celcius
- //Temp = (Temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
- return Temp;
-}
+
