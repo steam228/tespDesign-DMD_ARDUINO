84a6d8a7f2e531bb849cd7b9784c583e4c18c903
André Rocha <steam228@gmail.com>
2016-11-03 10:55:53 +0000

    Nao Racio Temperatura aula

---
+++ _09_SensorNaoRacio_Calibrar/_09_SensorNaoRacio_Calibrar.ino
@@ -0,0 +1,46 @@
+//TESP DMD - Computaçao Física
+// Ligação Thermistor 10K
+// retirado de:
+//http://playground.arduino.cc/ComponentLib/Thermistor2
+// (Ground) ---- (10k-Resistor) -------|------- (Thermistor) ---- (+5v)
+//                                     |
+//                                Analog Pin 0
+
+#include <math.h>
+
+#define portaTherm A0
+
+int valorTherm = 0;
+int Temperatura = 0;
+
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {
+
+valorTherm = analogRead(portaTherm);
+Temperatura = Thermistor(valorTherm);
+
+Serial.print("Temperatura: ");
+Serial.print(Temperatura);
+Serial.println(" celsius");
+
+delay(20);
+
+
+}
+
+
+
+
+double Thermistor(int RawADC) {
+ double Temp;
+ Temp = log(10000.0*((1024.0/RawADC-1))); 
+//         =log(10000.0/(1024.0/RawADC-1)) // for pull-up configuration
+ Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
+ Temp = Temp - 273.15;            // Convert Kelvin to Celcius
+ //Temp = (Temp * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
+ return Temp;
+}
